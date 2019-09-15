7d3e7508fd31801be73625087ef2ddb4176915e2
Eriksson Mikael <mikael.eriksson@digia.com>
2019-09-16 02:24:48 +0300

    Apply powered by logo to sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,22 +8,32 @@ import { Link } from "react-router-dom";
 const Sidebar = ({ isOpen, toggleSideBar }) => {
   return (
     <div className={classNames(css.sidebar, { [css.open]: isOpen })}>
-      <Link to="/search" className={css.row} onClick={() => toggleSideBar()}>
-        <MdSearch />
-        <div className={css.rowText}>Search</div>
-      </Link>
-      <Link to="/queue" className={css.row} onClick={() => toggleSideBar()}>
-        <MdQueueMusic />
-        <div className={css.rowText}>Queue</div>
-      </Link>
-      <Link to="/playlists" className={css.row} onClick={() => toggleSideBar()}>
-        <MdFormatListBulleted />
-        <div className={css.rowText}>Playlists</div>
-      </Link>
-      <Link to="/login" className={css.row} onClick={() => toggleSideBar()}>
-        <IoIosLogIn />
-        <div className={css.rowText}>Login</div>
-      </Link>
+      <div>
+        <Link to="/search" className={css.row} onClick={() => toggleSideBar()}>
+          <MdSearch />
+          <div className={css.rowText}>Search</div>
+        </Link>
+        <Link to="/queue" className={css.row} onClick={() => toggleSideBar()}>
+          <MdQueueMusic />
+          <div className={css.rowText}>Queue</div>
+        </Link>
+        <Link
+          to="/playlists"
+          className={css.row}
+          onClick={() => toggleSideBar()}
+        >
+          <MdFormatListBulleted />
+          <div className={css.rowText}>Playlists</div>
+        </Link>
+        <Link to="/login" className={css.row} onClick={() => toggleSideBar()}>
+          <IoIosLogIn />
+          <div className={css.rowText}>Login</div>
+        </Link>
+      </div>
+      <div className={css.bottom}>
+        Powered by
+        <img src="Spotify_Logo_RGB_Green.png" alt="" className={css.spotifyLogo} />
+      </div>
     </div>
   );
 };
