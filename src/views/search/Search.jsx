899dddd127de77247bc65618d2aea9afc0f3e6d4
Eriksson Mikael <mikael.eriksson@digia.com>
2019-09-16 02:26:53 +0300

    Add basic search logic

--- src/views/search/Search.jsx
+++ src/views/search/Search.jsx
@@ -1,8 +1,56 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import css from "./Search.module.css";
+import SearchResults from "./SearchResults";
 
 const Search = () => {
-  return <div className={css.view}>Search</div>;
+  const [searchInput, setSearchInput] = useState("");
+  const [searchTimeout, setSearchTimeout] = useState(undefined);
+  const [searchResults, setSearchResults] = useState("");
+
+  const fetchResults = async searchString => {
+    if (!searchString) return;
+    const response = await fetch(
+      "https://api.spotify.com/v1/search" +
+        `?q=${encodeURIComponent(searchString)}` +
+        "&type=album,artist,playlist,track" +
+        "&limit=3",
+      {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("access_token")}`
+        }
+      }
+    );
+    const results = await response.json();
+    if (results) {
+      setSearchResults(results);
+    }
+  };
+
+  const updateSearch = async value => {
+    setSearchInput(value);
+    clearTimeout(searchTimeout);
+
+    const newSearch = setTimeout(() => {
+      if (!value) return;
+      fetchResults(value);
+    }, 500);
+
+    setSearchTimeout(newSearch);
+  };
+
+  return (
+    <div className={css.view}>
+      <div className={css.title}>Search</div>
+      <input
+        type="text"
+        className={css.searchInput}
+        value={searchInput}
+        onChange={e => updateSearch(e.target.value)}
+        placeholder="Artists, albums, tracks, playlists"
+      />
+      <SearchResults results={searchResults} />
+    </div>
+  );
 };
 
 export default Search;
