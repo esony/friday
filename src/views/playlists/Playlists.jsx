fbbe1a25f38d4007b2744cd038bd33a123a097e7
Eriksson Mikael <mikael.eriksson@digia.com>
2019-09-15 17:24:37 +0300

    Add error handling to playlist fetch

--- src/views/playlists/Playlists.jsx
+++ src/views/playlists/Playlists.jsx
@@ -5,13 +5,17 @@ const Search = () => {
   const [userPlaylists, setUserPlaylists] = useState([]);
 
   const fetchPlaylists = async () => {
-    const response = await fetch("https://api.spotify.com/v1/me/playlists", {
-      headers: {
-        Authorization: `Bearer ${localStorage.getItem("access_token")}`
-      }
-    });
-    const { items } = await response.json();
-    setUserPlaylists(items);
+    try {
+      const response = await fetch("https://api.spotify.com/v1/me/playlists", {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("access_token")}`
+        }
+      });
+      const { items } = await response.json();
+      items && setUserPlaylists(items);
+    } catch (e) {
+      console.log(e);
+    }
   };
 
   useEffect(() => {
